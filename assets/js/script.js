1f55fb6a955a50ba638d2df527b5d45033560cff
Cian Lane <cianlane@gmail.com>
2023-01-08 20:34:47 +0000

    fix table tags. add inital js

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+
+
+let gradeTableData = {}
+
+
+function defaultTable() {
+
+}
+//Store table values in gradeTableData object
+function readTableValues() {
+    let table = document.getElementByID('grade-table');
+    let rows = table.children.children;
+    for (let row of rows) {
+        console.log(row)
+    }
+}
+
+function calculateResults() {
+
+}
+
+function insertResults() {
+
+}
+
+function expandOrContractDiv() {
+
+}
+
