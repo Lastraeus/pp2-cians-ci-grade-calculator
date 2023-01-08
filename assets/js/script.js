52cc4108c80b2066a4cebf9822346b7a4a8d6b90
Cian Lane <cianlane@gmail.com>
2023-01-08 20:48:23 +0000

    add table scrape js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 
 
-let gradeTableData = {}
+let gradeTableData = []
 
 
 function defaultTable() {
@@ -8,10 +8,20 @@ function defaultTable() {
 }
 //Store table values in gradeTableData object
 function readTableValues() {
-    let table = document.getElementByID('grade-table');
-    let rows = table.children.children;
+    let table = document.getElementById('grade-table-body');
+    let rows = table.children;
     for (let row of rows) {
-        console.log(row)
+        let projectLine = {}
+
+        let cells = row.children;
+        projectLine.project = cells[0].textContent;
+        projectLine.credits = cells[1].textContent;
+        projectLine.grade = cells[2].textContent;
+        projectLine.points = cells[3].textContent;
+        projectLine.total = cells[4].textContent;
+        gradeTableData.push(projectLine);
+
+        console.log(gradeTableData)
     }
 }
 
@@ -27,3 +37,11 @@ function expandOrContractDiv() {
 
 }
 
+function insertHtmlToExpandedDiv() {
+
+}
+
+// Wait for the DOM to finish loading before running
+document.addEventListener("DOMContentLoaded", readTableValues())
+
+readTableValues()
