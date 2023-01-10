bea995f1202135121a92f5be4d4792246c5a18ae
Cian Lane <cianlane@gmail.com>
2023-01-10 14:27:29 +0000

    add test js functionality for reading tables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,57 @@
-
-
 let gradeTableData = []
 const tableBody = document.getElementById('grade-table-body');
-const lineOneGrade = document.getElementById('grade1');
 const gradeSelectors = document.querySelectorAll(".grade-select")
 
-function defaultTable() {
-
+function firstTableRead() {
+    if (gradeTableData.length === 0) {
+        gradeSelectors.forEach(grade => {
+            gradeTableData.push(grade.value);
+        })
+        console.log(gradeTableData)
+    }
 }
+
 //Store table values in gradeTableData object
 function readTableGrade() {
 
+    function updateDataArray(selectedString) {
+        console.log(selectedString);
+    }
+
+    /**
+     * This function cycles through each node in the nodelist of grade selectors
+     * and makes a new unique instance of GradeChangeInterpreter class from it's id attribute.
+     */
+    function readTableGradeAfterChange() {
+        gradeSelectors.forEach(grade => {
+            new GradeChangeInterpreter(grade.id);
+        })
+    }
+
+    /**
+     * This class creates individualised grade ID selectors, 
+     * assigns unique change listeners to them which
+     * currently logs updated values to console
+     * necessary so each has a unique output upon change
+     * */
+    class GradeChangeInterpreter {
+        constructor(gradeId) {
+            let specificGradeId = document.querySelector(`#${gradeId}`)
+
+            specificGradeId.addEventListener("change", e => {
+                updateDataArray(specificGradeId.value);
+            });
+        }
+    }
+    firstTableRead()
+    readTableGradeAfterChange()
 }
+
+
+
+
+// Wait for the DOM to finish loading before running
+document.addEventListener("DOMContentLoaded", readTableGrade());
 
 
 
@@ -30,22 +70,3 @@ function expandOrContractDiv() {
 function insertHtmlToExpandedDiv() {
 
 }
-
-// Wait for the DOM to finish loading before running
-document.addEventListener("DOMContentLoaded", readTableGrade());
-
-class GradeInterpreter  {
-    constructor(gradeId){
-        let specificGrade = document.querySelector(`#${gradeId}`)
-        console.log(specificGrade)
-        specificGrade.addEventListener( "change", e => {
-        console.log(specificGrade.value);
-    } );
-   }
-
- }
-
- gradeSelectors.forEach(grade => {
-    new GradeInterpreter(grade.id);
- })
-//https://www.fwait.com/how-to-trigger-select-change-event-in-javascript/
