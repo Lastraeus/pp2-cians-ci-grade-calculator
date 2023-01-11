b6ad02a4626266b0757b5481ff6f674b9893487a
Cian Lane <cianlane@gmail.com>
2023-01-11 13:47:11 +0000

    edit official guide content/html

--- assets/js/insert-guide-script.js
+++ assets/js/insert-guide-script.js
@@ -4,17 +4,15 @@ let articleElement = document.querySelector("#assesment-quote")
 let defaultArticle = articleElement.innerHTML;
 
 function startAssesmentGuideSystem() {
-    rulesDiv.addEventListener("click", e => 
-    expandOrContractDiv())
+    rulesDiv.addEventListener("click", e =>
+        expandOrContractDiv())
 }
 
 function expandOrContractDiv() {
     if (!(expanded)) { // if div is not expanded (default)
-        console.log("clicked")
-        articleElement.innerHTML = htmlBlock;
+        articleElement.innerHTML = expandedHtmlBlock;
         expanded = true;
     } else {
-        console.log("defaultArticle")
         articleElement.innerHTML = defaultArticle;
         expanded = false;
     }
@@ -25,393 +23,148 @@ function insertHtmlToExpandedDiv() {}
 // Wait for the DOM to finish loading, then run the initial system
 document.addEventListener("DOMContentLoaded", startAssesmentGuideSystem());
 
-let htmlBlock = `<p>
-<strong>Final Grade Calculation</strong>
-</p>
+let expandedHtmlBlock = `
+<h2>
+    Final Grade Calculation As Per Official Assessment Guide
+</h2>
+<br>
+<h3 id="click-instruction">
+    Click anywhere in this box to shrink this again.
+</h3>
+<br>
 <p>
-The five projects all contribute to your final grade. This is calculated
-from the points awarded for a pass/merit/distinction multiplied by the
-number of credit points per module. These are depicted in the table below.
-The maximum number of points available are 480.
+    The five projects all contribute to your final grade.
+    <br>
+    This is calculated from the points awarded for a pass/merit/distinction multiplied by the
+    number of credit points per module.
+    <br>
+    These are depicted in the table below.
+    The maximum number of points available are 480.
 </p>
-<table cellpadding="0" border="0">
-<tbody>
-    <tr>
-        <td>
-            <p>
-                <strong>Module</strong>
-            </p>
-        </td>
-        <td>
-            <p>
-                <strong>Credit Points</strong>
-            </p>
-        </td>
-    </tr>
-    <tr>
-        <td width="50%">
-            <p>
-                HTML/CSS Essentials
-            </p>
-        </td>
-        <td>
-            <p>
+<br>
+<table id="credit-explanation-table " class="center">
+    <thead>
+        <th>Module</th>
+        <th>Credit Points</th>
+    </thead>
+    <tbody>
+        <tr>
+            <td>
+                PP1 - HTML/CSS Essentials
+            </td>
+            <td>
                 8
-            </p>
-        </td>
-    </tr>
-    <tr>
-        <td>
-            <p>
-                Javascript Essentials
-            </p>
-        </td>
-        <td>
-            <p>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                PP2 - Javascript Essentials
+            </td>
+            <td>
                 8
-            </p>
-        </td>
-    </tr>
-    <tr>
-        <td>
-            <p>
-                Python Essentials
-            </p>
-        </td>
-        <td>
-            <p>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                PP3 - Python Essentials
+            </td>
+            <td>
                 8
-            </p>
-        </td>
-    </tr>
-    <tr>
-        <td>
-            <p>
-                Full Stack Toolkit
-            </p>
-        </td>
-        <td>
-            <p>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                PP4 - Full Stack Toolkit
+            </td>
+            <td>
                 16
-            </p>
-        </td>
-    </tr>
-    <tr>
-        <td>
-            <p>
-                Chosen Specialisation
-            </p>
-        </td>
-        <td>
-            <p>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                PP5 - Chosen Specialisation
+            </td>
+            <td>
                 20
-            </p>
-        </td>
-    </tr>
-</tbody>
+            </td>
+        </tr>
+    </tbody>
 </table>
-<table cellpadding="0" border="0">
-<tbody>
-    <tr>
-        <td colspan="2">
-            <p>
-                <strong>Points Per Grade</strong>
-            </p>
-        </td>
-    </tr>
-    <tr>
-        <td width="50%">
-            <p>
+<br>
+<table id="points-per-grade-table" class="center">
+    <thead>
+        <th>Points Per Grade</th>
+    </thead>
+    <tbody>
+        <tr>
+            <td>
                 Pass
-            </p>
-        </td>
-        <td>
-            <p>
+            </td>
+            <td>
                 4
-            </p>
-        </td>
-    </tr>
-    <tr>
-        <td>
-            <p>
+            </td>
+        </tr>
+        <tr>
+            <td>
                 Merit
-            </p>
-        </td>
-        <td>
-            <p>
+            </td>
+            <td>
                 6
-            </p>
-        </td>
-    </tr>
-    <tr>
-        <td>
-            <p>
+            </td>
+        </tr>
+        <tr>
+            <td>
                 Distinction
-            </p>
-        </td>
-        <td>
-            <p>
+            </td>
+            <td>
                 8
-            </p>
-        </td>
-    </tr>
-</tbody>
+            </td>
+        </tr>
+    </tbody>
 </table>
-<table cellpadding="0" border="0">
-<tbody>
-    <tr>
-        <td colspan="2">
-            <p>
-                <strong>Grade Bands</strong>
-            </p>
-        </td>
-    </tr>
-    <tr>
-        <td width="50%">
-            <p>
-                Pass
-            </p>
-        </td>
-        <td>
-            <p>
-                240 - 311
-            </p>
-        </td>
-    </tr>
-    <tr>
-        <td>
-            <p>
-                Merit
-            </p>
-        </td>
-        <td>
-            <p>
-                312 - 383
-            </p>
-        </td>
-    </tr>
-    <tr>
-        <td>
-            <p>
-                Distinction
-            </p>
-        </td>
-        <td>
-            <p>
-                384 - 480
-            </p>
-        </td>
-    </tr>
-</tbody>
+<br>
+<table id="grade-bands-table" class="center">
+    <thead>
+    <th>Grade Bands</th>
+    </thead>
+    <tbody>
+        <tr>
+            <td>
+                <p>
+                    Pass
+                </p>
+            </td>
+            <td>
+                <p>
+                    240 - 311
+                </p>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <p>
+                    Merit
+                </p>
+            </td>
+            <td>
+                <p>
+                    312 - 383
+                </p>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <p>
+                    Distinction
+                </p>
+            </td>
+            <td>
+                <p>
+                    384 - 480
+                </p>
+            </td>
+        </tr>
+    </tbody>
 </table>
-<table cellpadding="0" border="0">
-<tbody>
-    <tr>
-        <td>
-            <p>
-                <strong>Sample </strong>
-            </p>
-        </td>
-        <td>
-            <p>
-                <strong>Credits</strong>
-            </p>
-        </td>
-        <td>
-            <p>
-                <strong>Grade</strong>
-            </p>
-        </td>
-        <td>
-            <p>
-                <strong>Points </strong>
-            </p>
-        </td>
-        <td>
-            <p>
-                <strong>Total</strong>
-            </p>
-        </td>
-    </tr>
-    <tr>
-        <td width="50%">
-            <p>
-                HTML/CSS Essentials
-            </p>
-        </td>
-        <td>
-            <p>
-                8
-            </p>
-        </td>
-        <td>
-            <p>
-                Pass
-            </p>
-        </td>
-        <td>
-            <p>
-                4
-            </p>
-        </td>
-        <td>
-            <p>
-                32
-            </p>
-        </td>
-    </tr>
-    <tr>
-        <td>
-            <p>
-                Javascript Essentials
-            </p>
-        </td>
-        <td>
-            <p>
-                8
-            </p>
-        </td>
-        <td>
-            <p>
-                Merit
-            </p>
-        </td>
-        <td>
-            <p>
-                6
-            </p>
-        </td>
-        <td>
-            <p>
-                48
-            </p>
-        </td>
-    </tr>
-    <tr>
-        <td>
-            <p>
-                Python Essentials
-            </p>
-        </td>
-        <td>
-            <p>
-                8
-            </p>
-        </td>
-        <td>
-            <p>
-                Merit
-            </p>
-        </td>
-        <td>
-            <p>
-                6
-            </p>
-        </td>
-        <td>
-            <p>
-                48
-            </p>
-        </td>
-    </tr>
-    <tr>
-        <td>
-            <p>
-                Full Stack Toolkit
-            </p>
-        </td>
-        <td>
-            <p>
-                16
-            </p>
-        </td>
-        <td>
-            <p>
-                Pass
-            </p>
-        </td>
-        <td>
-            <p>
-                4
-            </p>
-        </td>
-        <td>
-            <p>
-                64
-            </p>
-        </td>
-    </tr>
-    <tr>
-        <td>
-            <p>
-                Chosen Specialisation
-            </p>
-        </td>
-        <td>
-            <p>
-                20
-            </p>
-        </td>
-        <td>
-            <p>
-                Pass
-            </p>
-        </td>
-        <td>
-            <p>
-                4
-            </p>
-        </td>
-        <td>
-            <p>
-                80
-            </p>
-        </td>
-    </tr>
-    <tr>
-        <td>
-            <p>
-                Total
-            </p>
-        </td>
-        <td>
-            <p>
-                60
-            </p>
-        </td>
-        <td>
-            <br/>
-        </td>
-        <td>
-            <br/>
-        </td>
-        <td>
-            <p>
-                272
-            </p>
-        </td>
-    </tr>
-    <tr>
-        <td>
-            <br/>
-        </td>
-        <td>
-            <br/>
-        </td>
-        <td>
-            <br/>
-        </td>
-        <td>
-            <p>
-                Final Grade
-            </p>
-        </td>
-        <td>
-            <p>
-                Pass
-            </p>
-        </td>
-    </tr>
-</tbody>
-</table>
+<br>
 `
